303d83215fec19e080304d81a41696debcc31c6b
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-17 16:58:51 -0300

    fix: code position in index.css & add components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
-      <main className='h-screen flex justify-center items-center px-10'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fugit natus animi deleniti ducimus molestias quae, eveniet mollitia, nobis enim
-        beatae sed quis, deserunt dolores quas ut! Ex, architecto debitis?
-      </main>
+      <Hero />
+      <Projects />
       <Footer />
     </>
   );
