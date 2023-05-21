49459aeb18a8b43170e17f3f6766f1e3a9c173d8
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-21 14:13:23 -0300

    fix: fixed wrong CSS

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       className='h-screen w-full bg-gradient-to-b from-mistGray-50 from-50% to-mistGray-950'>
       <div className='container sm:pt-32 max-sm:pt-16 max-xs:pt-6 flex justify-between items-center flex-wrap'>
         {/* Hero info */}
-        <div className='flex flex-col flex-start leading-none text-mistGray-950'>
+        <div className='flex flex-col items-start leading-none text-mistGray-950'>
           <div>
             <h1 className='uppercase font-Kanit font-semibold tracking-wider text-5xl'>
               <span className='normal-case font-Robot text-[1.25rem] font-normal tracking-normal -mr-2'>
