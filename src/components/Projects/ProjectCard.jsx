69ee6eac04b7a18ebb7ca2b70e3b56400d89cbc0
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-28 14:43:18 -0300

    Projects and ProjectCard rebuilt, add new project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
+import { FaCode, FaRegPlayCircle } from 'react-icons/fa';
 
-const ProjectCard = ({ children, imgSrc, alt }) => {
+const ProjectCard = ({
+  projectTitle,
+  projectDesc,
+  projectTech,
+  projectImg,
+  projectAlt,
+  projectRepo,
+  projectDemo,
+}) => {
   return (
-    <div className='flex flex-col justify-between rounded-lg overflow-hidden shadow-lg bg-mistGray-50'>
-      <img className='w-full object-cover' src={imgSrc} alt={alt} />
-      <div className='px-6 py-4'>{children}</div>
+    <div className='flex flex-wrap items-center max-md:px-4'>
+      <div className='overflow-hidden shadow-lg transition duration-500 ease-in-out transform hover:scale-105 hover:shadow-2xl rounded-lg md:w-80 w-full'>
+        <img
+          alt={projectAlt}
+          src={projectImg}
+          className='max-h-50 w-full object-cover'
+        />
+        <div className='bg-mistGray-50 w-full p-4'>
+          <h3 className='font-Kanit text-mistGray-950 text-2xl font-semibold tracking-wide'>
+            <span className='text-rioGrande-600 mr-1'>.</span>
+            {projectTitle}
+          </h3>
+          <p className='text-mistGray-700 font-light text-sm leading-relaxed'>
+            {projectDesc}
+          </p>
+          <div className='flex flex-wrap justify-starts items-center py-3 border-b-2 text-xs text-white font-medium'>
+            {projectTech.map((tech) => (
+              <span
+                key={tech}
+                className='m-1 px-2 py-1 rounded bg-mistGray-500 font-semibold'>
+                {tech.charAt(0).toUpperCase() + tech.slice(1)}
+              </span>
+            ))}
+          </div>
+          <div className='flex justify-between items-center'>
+            <a
+              href={projectRepo}
+              target='_blank'
+              rel='noreferrer noopener'
+              className={`flex items-center gap-2 cursor-pointer mt-3 px-2 py-1 rounded-lg bg-royalBlue-500 
+              border-2 border-royalBlue-600 text-mistGray-50 text-sm font-semibold tracking-wide
+              
+              hover:bg-mistGray-50 hover:border-royalBlue-500 hover:text-royalBlue-500`}>
+              Repository <FaCode />
+            </a>
+            <a
+              href={projectDemo}
+              target='_blank'
+              rel='noreferrer noopener'
+              className={`flex items-center gap-2 cursor-pointer mt-3 px-2 py-1 rounded-lg bg-royalBlue-500 
+              border-2 border-royalBlue-600 text-mistGray-50 text-sm font-semibold tracking-wide
+              
+              hover:bg-mistGray-50 hover:border-royalBlue-500 hover:text-royalBlue-500`}>
+              Demo <FaRegPlayCircle />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,81 +1,57 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import ProjectCard from './ProjectCard';
-import { FaCode, FaRegPlayCircle } from 'react-icons/fa';
 import ProjectOne from '../../assets/img/projectsImages/steelManeMotors.jpg';
 import ProjectTwo from '../../assets/img/projectsImages/surfbot.jpg';
+import ProjectThree from '../../assets/img/projectsImages/animais-fantasticos.jpg';
 
 const Projects = () => {
   return (
     <Element name='projects'>
       <section aria-label='Project section' className='bg-mistGray-900'>
         <div className='container max-w-[1440px] sm:py-32 max-sm:py-16 max-xs:py-6'>
-          <div className='p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5'>
-            {/* CARD 1 */}
-            <ProjectCard imgSrc={ProjectOne} alt='SteelMane Motorcycles'>
-              <h3 className='font-Kanit font-semibold text-xl text-mistGray-950 mb-2'>
-                SteelMane Motorcycles
-              </h3>
-              <p className='text-mistGray-950 '>
-                This website was developed during the "UI Design for Beginners"
-                and the "HTML and CSS for Beginners" courses from Origamid. The
-                brand is entirely fictional, with the purpose of showcasing
-                customized and totally original motorcycles.
-              </p>
-              <div className='px-6 pt-4 pb-2'>
-                <span className='block bg-royalBlue-700 rounded-full px-3 py-1 text-sm font-semibold text-mistGray-50 border-2 border-mistGray-50 mr-2 mb-2 hover:bg-mistGray-50 hover:text-royalBlue-700 hover:border-2 hover:border-royalBlue-700'>
-                  <a
-                    href='https://github.com/Barata-Ribeiro/steelmane-motorcycles'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='flex justify-center items-center gap-2 cursor-pointer'>
-                    Repository <FaCode />
-                  </a>
-                </span>
-                <span className='block bg-royalBlue-700 rounded-full px-3 py-1 text-sm font-semibold text-mistGray-50 border-2 border-mistGray-50 mr-2 mb-2 hover:bg-mistGray-50 hover:text-royalBlue-700 hover:border-2 hover:border-royalBlue-700'>
-                  <a
-                    href='https://barata-ribeiro.github.io/steelmane-motorcycles/'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='flex justify-center items-center gap-2 cursor-pointer'>
-                    Demo <FaRegPlayCircle />
-                  </a>
-                </span>
-              </div>
-            </ProjectCard>
-
-            {/* CARD 2 */}
-            <ProjectCard imgSrc={ProjectTwo} alt='Surfbot'>
-              <h3 className='font-Kanit font-semibold text-xl text-mistGray-950 mb-2'>
-                Surfbot
-              </h3>
-              <p className='text-mistGray-950 '>
-                This is a website developed from scratch for a fictional surfing
-                school. The design of the website was created during the
-                "Advanced UI Design" course from Origamid. I wrote the code on
-                my own to consolidate the learning process from other courses.
-              </p>
-              <div className='px-6 pt-4 pb-2'>
-                <span className='block bg-royalBlue-700 rounded-full px-3 py-1 text-sm font-semibold text-mistGray-50 border-2 border-mistGray-50 mr-2 mb-2 hover:bg-mistGray-50 hover:text-royalBlue-700 hover:border-2 hover:border-royalBlue-700'>
-                  <a
-                    href='https://github.com/Barata-Ribeiro/surfbot'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='flex justify-center items-center gap-2 cursor-pointer'>
-                    Repository <FaCode />
-                  </a>
-                </span>
-                <span className='block bg-royalBlue-700 rounded-full px-3 py-1 text-sm font-semibold text-mistGray-50 border-2 border-mistGray-50 mr-2 mb-2 hover:bg-mistGray-50 hover:text-royalBlue-700 hover:border-2 hover:border-royalBlue-700'>
-                  <a
-                    href='https://barata-ribeiro.github.io/surfbot/'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='flex justify-center items-center gap-2 cursor-pointer'>
-                    Demo <FaRegPlayCircle />
-                  </a>
-                </span>
-              </div>
-            </ProjectCard>
+          <div className='flex justify-evenly items-center flex-wrap gap-8'>
+            <ProjectCard
+              projectTitle={`SteelMane Motorcycles`}
+              projectDesc={
+                'Project developed during the "UI Design for Beginners" and the "HTML and CSS for Beginners" courses from Origamid. The SteelMane Motorcycles brand i entirely fictional, with the purpose of showcasing customized and totally original motorcycles.'
+              }
+              projectImg={ProjectOne}
+              projectAlt={'SteelMane Motorcycles'}
+              projectTech={['HTML', 'CSS', 'JavaScript']}
+              projectRepo={
+                'https://github.com/Barata-Ribeiro/steelmane-motorcycles'
+              }
+              projectDemo={
+                'https://barata-ribeiro.github.io/steelmane-motorcycles/'
+              }
+            />
+            <ProjectCard
+              projectTitle={`Surfbot`}
+              projectDesc={
+                'Project developed from scratch for a fictional surfing school. The design was created during the "Advanced UI Design" course from Origamid. I wrote the code on my own to consolidate the learning process from other classes.'
+              }
+              projectImg={ProjectTwo}
+              projectAlt={'Surfbot'}
+              projectTech={['HTML', 'CSS', 'JavaScript']}
+              projectRepo={'https://github.com/Barata-Ribeiro/surfbot'}
+              projectDemo={'https://barata-ribeiro.github.io/surfbot/'}
+            />
+            <ProjectCard
+              projectTitle={'Fantastic Beasts'}
+              projectDesc={
+                'Project developed during the JavaScript ES6+ course at Origamid. This project demonstrates a mastery of front-end development with JavaScript, showcasing creativity and practical application of programming knowledge.'
+              }
+              projectImg={ProjectThree}
+              projectAlt={'Fantastic Beasts'}
+              projectTech={['HTML', 'CSS', 'JavaScript']}
+              projectRepo={
+                'https://github.com/Barata-Ribeiro/animais-fantasticos'
+              }
+              projectDemo={
+                'https://barata-ribeiro.github.io/animais-fantasticos/'
+              }
+            />
           </div>
         </div>
       </section>
