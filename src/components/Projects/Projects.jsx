e35ccbb72fa641221e659eb78e3bf4bef4a99f15
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-05-30 08:38:51 -0300

    rearranged projects listing

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,6 +12,32 @@ const Projects = () => {
         <div className='container max-w-[1440px] sm:py-32 max-sm:py-16 max-xs:py-6'>
           <div className='flex justify-evenly items-center flex-wrap gap-8'>
             <ProjectCard
+              projectTitle={'Fantastic Beasts'}
+              projectDesc={
+                'Project developed during the JavaScript ES6+ course at Origamid. This project demonstrates a mastery of front-end development with JavaScript, showcasing creativity and practical application of programming knowledge.'
+              }
+              projectImg={ProjectThree}
+              projectAlt={'Fantastic Beasts'}
+              projectTech={['JavaScript', 'HTML', 'CSS']}
+              projectRepo={
+                'https://github.com/Barata-Ribeiro/animais-fantasticos'
+              }
+              projectDemo={
+                'https://barata-ribeiro.github.io/animais-fantasticos/'
+              }
+            />
+            <ProjectCard
+              projectTitle={`Surfbot`}
+              projectDesc={
+                'Project developed from scratch for a fictional surfing school. The design was created during the "Advanced UI Design" course from Origamid. I wrote the code on my own to consolidate the learning process from other classes.'
+              }
+              projectImg={ProjectTwo}
+              projectAlt={'Surfbot'}
+              projectTech={['JavaScript', 'HTML', 'CSS']}
+              projectRepo={'https://github.com/Barata-Ribeiro/surfbot'}
+              projectDemo={'https://barata-ribeiro.github.io/surfbot/'}
+            />
+            <ProjectCard
               projectTitle={`SteelMane Motorcycles`}
               projectDesc={
                 'Project developed during the "UI Design for Beginners" and the "HTML and CSS for Beginners" courses from Origamid. The SteelMane Motorcycles brand i entirely fictional, with the purpose of showcasing customized and totally original motorcycles.'
@@ -24,32 +50,6 @@ const Projects = () => {
               }
               projectDemo={
                 'https://barata-ribeiro.github.io/steelmane-motorcycles/'
-              }
-            />
-            <ProjectCard
-              projectTitle={`Surfbot`}
-              projectDesc={
-                'Project developed from scratch for a fictional surfing school. The design was created during the "Advanced UI Design" course from Origamid. I wrote the code on my own to consolidate the learning process from other classes.'
-              }
-              projectImg={ProjectTwo}
-              projectAlt={'Surfbot'}
-              projectTech={['HTML', 'CSS', 'JavaScript']}
-              projectRepo={'https://github.com/Barata-Ribeiro/surfbot'}
-              projectDemo={'https://barata-ribeiro.github.io/surfbot/'}
-            />
-            <ProjectCard
-              projectTitle={'Fantastic Beasts'}
-              projectDesc={
-                'Project developed during the JavaScript ES6+ course at Origamid. This project demonstrates a mastery of front-end development with JavaScript, showcasing creativity and practical application of programming knowledge.'
-              }
-              projectImg={ProjectThree}
-              projectAlt={'Fantastic Beasts'}
-              projectTech={['HTML', 'CSS', 'JavaScript']}
-              projectRepo={
-                'https://github.com/Barata-Ribeiro/animais-fantasticos'
-              }
-              projectDemo={
-                'https://barata-ribeiro.github.io/animais-fantasticos/'
               }
             />
           </div>
