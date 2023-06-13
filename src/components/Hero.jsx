7a2d84c0f32928ce890098bcfefc66a1b4666e64
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-13 17:49:04 -0300

    feat: final hero main style, add main button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Element } from 'react-scroll';
 import Avatar from '/public/assets/img/avatar.jpg';
+import MainButton from './utils/Button';
 
 const Hero = () => {
   const avatarStyle = {
     height: '6rem',
     width: '6rem',
-    margin: '0 auto 0.5rem auto',
     background: `url(${Avatar}) no-repeat center/cover`,
     borderRadius: '50%',
   };
@@ -16,9 +16,9 @@ const Hero = () => {
       <main className='' aria-label='Site home page'>
         <div className={`mt-[13rem] mb-[7.5rem]`}>
           {/* Hero info */}
-          <div className='flex flex-col items-start leading-none text-mistGray-950 text-center'>
+          <div className='flex flex-col items-center leading-none text-mistGray-950 text-center gap-6'>
+            <div style={avatarStyle}></div>
             <div>
-              <div style={avatarStyle}></div>
               <h1 className='uppercase font-Kanit font-semibold tracking-wider text-5xl text-mistGray-950'>
                 <span className='normal-case font-Robot text-[0.875rem] font-normal tracking-normal -mr-2'>
                   I'm
@@ -34,6 +34,11 @@ const Hero = () => {
                 appealing, user-friendly interfaces.
               </p>
             </div>
+            <MainButton
+              buttonText='GitHub'
+              href='https://github.com/Barata-Ribeiro'
+              title='GitHub'
+            />
           </div>
         </div>
       </main>
