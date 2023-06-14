5391f2dd05db7e6a52b7546c7b9f3e08bdad6b7f
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-14 10:46:51 -0300

    feat: add slider carousel and its images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Element } from 'react-scroll';
+import ProjectOne from '/public/assets/img/projects/surfbot.jpg';
+import ProjectTwo from '/public/assets/img/projects/steelManeMotors.jpg';
+import ProjectThree from '/public/assets/img/projects/animaisFantasticos.jpg';
+import ProjectFour from '/public/assets/img/projects/gitpop.jpg';
+import ProjectFive from '/public/assets/img/projects/spectreBand.jpg';
 import Slider from './Slider';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Projects = () => {
+  const slides = [
+    { id: 1, src: ProjectOne, alt: 'Surfbot' },
+    { id: 2, src: ProjectTwo, alt: 'SteelMane Motorcycles' },
+    { id: 3, src: ProjectThree, alt: 'Animais Fantásticos' },
+    { id: 4, src: ProjectFour, alt: 'GitPop' },
+    { id: 5, src: ProjectFive, alt: 'Spectre' },
+  ];
+
   return (
     <>
       <Element name='projects'>
         <section className='mt-[4.5rem] mb-[7.5rem]'>
-          <div className='mb-6'>
+          <div className='flex flex-col items-center mb-6'>
             <h1 className='font-Kanit text-mistGray-950 text-6xl text-center'>
               <span className='text-rioGrande-600'>.</span>My projects
             </h1>
@@ -18,7 +33,24 @@ const Projects = () => {
               field.
             </p>
           </div>
-          <Slider />
+          <div className='max-w-[1400px]'>
+            <Slider>
+              {slides.map((slide) => (
+                <LazyLoadImage
+                  key={slide.id}
+                  src={slide.src}
+                  alt={slide.alt}
+                  height={600}
+                  width={1400}
+                  className='max-w-[1400px] flex-shrink-0 rounded-2xl'
+                  style={{
+                    boxShadow: 'inset 0px 0px 50px -5px rgba(39, 38, 35, 0.25)',
+                  }}
+                  effect='blur'
+                />
+              ))}
+            </Slider>
+          </div>
         </section>
       </Element>
     </>
--- src/components/Projects/Slider.jsx
+++ src/components/Projects/Slider.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi2';
 
-const Slider = () => {
+const Slider = ({ children: slides }) => {
+  const buttonClasses = `p-5 rounded-full shadow-lg bg-royalBlue-600 text-royalBlue-950 hover:bg-mistGray-50 transition-colors`;
+
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const prevSlide = () =>
+    setCurrentSlide((currentSlide) =>
+      currentSlide === 0 ? slides.length - 1 : currentSlide - 1
+    );
+  const nextSlide = () =>
+    setCurrentSlide((currentSlide) =>
+      currentSlide === slides.length - 1 ? 0 : currentSlide + 1
+    );
+
   return (
-    <div>Slider</div>
-  )
-}
+    <div className='overflow-hidden relative'>
+      <div
+        className='flex transition-transform ease-out duration-500'
+        style={{
+          transform: `translateX(-${currentSlide * 100}%)`,
+        }}>
+        {slides}
+      </div>
+      <div className='absolute inset-0 flex items-center justify-between p-28'>
+        <button onClick={prevSlide} className={buttonClasses}>
+          <HiOutlineChevronLeft size={40} />
+        </button>
+        <button onClick={nextSlide} className={buttonClasses}>
+          <HiOutlineChevronRight size={40} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
