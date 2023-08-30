0043bc04514475e0eec561ad4b41b539dd5facf9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-30 10:23:28 -0300

    feat: add data types

---
+++ src/types/IntensiveCourse.ts
@@ -0,0 +1,5 @@
+export interface IntensiveCourse {
+  id: string;
+  course: string;
+  place: string;
+}
---
+++ src/types/ProjectTypes.ts
@@ -0,0 +1,11 @@
+// src/types/ProjectTypes.ts
+export interface Project {
+  id: string;
+  title: string;
+  desc: string;
+  img: string;
+  alt: string;
+  tech: string[];
+  repo: string;
+  demo: string;
+}
