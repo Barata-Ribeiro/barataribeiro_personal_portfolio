5ea9f5f75e9be887d8be6978511b038e37e344c5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-08-30 10:24:07 -0300

    feat: add useFetch hook, started app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import useFetch from './hooks/useFetch';
+import { Project, IntensiveCourse } from './types';
+
+interface Data {
+  projects: Project[];
+  intensiveCourses: IntensiveCourse[];
+}
 
 const App = () => {
+  const { data, loading, error } = useFetch<Data>('/assets/data/data.json');
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <>
       <h1 className="text-5xl">Hello, World!</h1>
