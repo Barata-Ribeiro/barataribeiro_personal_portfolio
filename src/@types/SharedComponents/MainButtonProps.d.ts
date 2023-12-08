6361b51a5549b029629ded26095837f93fdceca3
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-12-08 10:50:51 -0300

    Reset Commit

--- src/@types/SharedComponents/MainButtonProps.d.ts
+++
@@ -1,8 +0,0 @@
-// types/MainButtonProps.ts
-export interface MainButtonProps {
-  buttonText: string;
-  href: string;
-  title: string;
-  target?: string;
-  rel?: string;
-}
--- src/@types/index.d.ts
+++
@@ -1,7 +0,0 @@
-// src/@types/index.ts
-export * from './ProjectTypes';
-export * from './EducationTypes';
-
-export interface ErrorProps {
-  message: string;
-}
