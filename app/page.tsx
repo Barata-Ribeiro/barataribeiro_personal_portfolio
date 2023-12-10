5a73da6c90b8826ec7b068d0a992cd95a53d3804
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-12-10 06:46:50 -0300

    refactor: change main container

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className='container'>
+      <main className='lg:container px-1'>
         <Hero />
         <About />
         <Projects />
