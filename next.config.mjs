88b4d9c5be53523b3250da5177cdd88419432663
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-11 09:14:38 -0300

    raw: modify prettier and next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 60
   },
   compiler: {
-    removeConsole: true,
+    removeConsole: true
   },
   compress: true,
   swcMinify: true,
   reactStrictMode: true,
+  trailingSlash: false,
+  skipTrailingSlashRedirect: true,
+  experimental: {
+    optimizePackageImports: [
+      "tailwindcss", "react-icons/fa", "react-icons/fa6", "react-icons/md"
+    ]
+  }
 }
 
 export default nextConfig
