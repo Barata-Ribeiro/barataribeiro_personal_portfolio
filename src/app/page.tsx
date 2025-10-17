476377d7c4fbb07efdb9f537e8308c486f3ebd55
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-17 10:32:24 -0300

    feat: implement Footer component with social links and navigation menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { FileData } from '@/app/types/index';
+import Footer from '@/components/layout/footer';
 import { readFile } from 'node:fs/promises';
 import path from 'node:path';
 
 export default async function Home() {
-    const filePath = path.join(process.cwd(), 'app', 'data', 'data.json');
+    const filePath = path.join(process.cwd(), 'src', 'app', 'data', 'data.json');
     const file = await readFile(filePath, 'utf8');
-    const { projects, higherEducationCourses, intensiveCourses }: FileData = JSON.parse(file);
 
-    return <main role="main" className="snap-y snap-mandatory"></main>;
+    const data: FileData = JSON.parse(file);
+
+    return (
+        <main role="main" className="snap-y snap-mandatory">
+            <Footer />
+        </main>
+    );
 }
