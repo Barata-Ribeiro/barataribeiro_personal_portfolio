d7c8b8eb4cae10f2811afa34b5b64294dffe1fb6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-10-17 15:14:37 -0300

    raw: add allowedDevOrigins configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from 'next';
 
+const allowedDevOrigins = process.env.ALLOWED_DEV_ORIGINS?.split(',') ?? [];
+
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    compiler: {
-        removeConsole: true,
+    allowedDevOrigins,
+    images: {
+        minimumCacheTTL: 31536000,
+        qualities: [50, 75, 100],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
     },
+    compiler: { removeConsole: true },
     experimental: {
         reactCompiler: true,
         optimizePackageImports: ['tailwindcss'],
